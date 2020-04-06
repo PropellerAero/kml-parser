ffd8cf207a40cf54fab6711ee167779f050a35b3
Tony Luk <impactblue@gmail.com>
2020-04-07 00:49:30 +1000

    Compact coordinates to not parse empty coordinates

--- src/parser/CoordinatesParser.ts
+++ src/parser/CoordinatesParser.ts
@@ -1,10 +1,11 @@
+import { compact } from 'lodash';
 import BaseParser from './BaseParser';
 import { Coordinate } from '../types/kml';
 import { valueToCoordinates } from './CoordinateParser';
 
 export default class CoordinatesParser extends BaseParser<Array<Coordinate>> {
     async text(value: string) {
-        const coordinates = value.split(' ').map((chunk) => {
+        const coordinates = compact(value.split(' ')).map((chunk) => {
             const coordinate = valueToCoordinates(chunk);
             if (this.options.convertCoordinate) {
                 return this.options.convertCoordinate(coordinate);
