d2bd928d7172009b124cde78ea6335137069dab2
Tony Luk <impactblue@gmail.com>
2020-04-06 16:11:51 +1000

    Clean up coordinates

--- src/parser/CoordinateParser.ts
+++ src/parser/CoordinateParser.ts
@@ -1,10 +1,17 @@
 import BaseParser from './BaseParser';
 import { Coordinate } from '../types/kml';
 
+const removeSuffix = (value: string) => value.replace(/['"]/g, '');
+
+export const valueToCoordinates = (value: string) => {
+    const [x, y, z = 0] = value
+        .split(',')
+        .map((value) => Number(removeSuffix(value)));
+    return { x, y, z };
+};
 export default class CoordinateParser extends BaseParser<Coordinate> {
     async text(value: string) {
-        const [x, y, z = 0] = value.split(',').map(Number);
-        let coordinate = { x, y, z };
+        let coordinate = valueToCoordinates(value);
         if (this.options.convertCoordinate) {
             coordinate = this.options.convertCoordinate(coordinate);
         }
--- src/parser/CoordinatesParser.ts
+++ src/parser/CoordinatesParser.ts
@@ -1,11 +1,11 @@
 import BaseParser from './BaseParser';
 import { Coordinate } from '../types/kml';
+import { valueToCoordinates } from './CoordinateParser';
 
 export default class CoordinatesParser extends BaseParser<Array<Coordinate>> {
     async text(value: string) {
         const coordinates = value.split(' ').map((chunk) => {
-            const [x, y, z] = chunk.split(',').map(Number);
-            const coordinate = { x, y, z };
+            const coordinate = valueToCoordinates(chunk);
             if (this.options.convertCoordinate) {
                 return this.options.convertCoordinate(coordinate);
             }
